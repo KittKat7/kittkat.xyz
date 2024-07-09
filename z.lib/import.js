fdfa70a4fdc5e5c457ca58c8f98c77bbe8ada5f1
KittKat <122490347+KittKat7@users.noreply.github.com>
2024-07-09 19:47:44 -0400

    Update to articles page

--- z.lib/import.js
+++ z.lib/import.js
@@ -1,13 +1,38 @@
 
-function include() {
+function include(loc) {
 	var scriptElement = document.currentScript;
 	var scriptId = scriptElement.id;
+	if (loc) scriptId = loc;
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", scriptId, true);
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState == 4 && xhr.status == 200) {
 			scriptElement.insertAdjacentHTML('beforebegin', xhr.responseText);
+		}
+	};
+	xhr.send();
+}
+
+function includeMD(loc) {
+	var scriptElement = document.currentScript;
+	var scriptId = scriptElement.id;
+	if (loc) scriptId = loc;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", scriptId, true);
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			scriptElement.insertAdjacentHTML('beforebegin',
+				xhr.responseText
+					.replace(/^### (.*$)/gim, '<h3>$1</h3>') // h3 tag
+					.replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
+					.replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
+					.replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
+					.replace(/\*(.*)\*/gim, '<i>$1</i>') // italic text);
+					.replace(/\n/gim, "<br>")
+					.trim()
+			)
 		}
 	};
 	xhr.send();
