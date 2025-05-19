5537d35163c24ced4907a1ab8a25f32e05538790
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-05-19 18:05:17 -0400

    Improved search ranking, and fixed title for home page

--- src/search/search.js
+++ src/search/search.js
@@ -1,13 +1,14 @@
 var index = null;
 
+// Fetch the search index json file
 async function fetchIndex() {
 	if (index != null) return;
 	const response = await fetch('/search/index.json');
 	const json = await response.json();
-	console.log("index loaded");
 	return new Map(Object.entries(json)); // Return the fetched data
 }
 
+// Adds items to the search list displayed on the page. If null is passed, will display no results.
 async function displayResult(result) {
 	var resultsDiv = document.getElementById("results");
 	var node = document.createElement("div");
@@ -19,7 +20,7 @@ async function displayResult(result) {
 		var link = document.createElement("a");
 		h.appendChild(link);
 		// link.classList.add("btn");
-		link.textContent = `${result["title"]} (${result["link"]})`;
+		link.textContent = `[${result["index"]}] ${result["title"]} (${result["link"]})`;
 		link.href = result["link"];
 		node.appendChild(h);
 		node.appendChild(document.createTextNode(result["desc"]));
@@ -28,6 +29,7 @@ async function displayResult(result) {
 	resultsDiv.appendChild(document.createElement("br"));
 }
 
+// Runs a search through the index for the provided search term. Uses displayResult to display the located search results.
 async function searchIndex(search) {
 	index = await fetchIndex();
 
@@ -40,62 +42,66 @@ async function searchIndex(search) {
 		return null;
 	}
 
-	var resultList = [];
-
-	console.log(search);
-	for (let [key, value] of index) {
-		console.log(key);
-		// title search
-		if (value["title"].toLowerCase().includes(search)) {
-			resultList.push({
-				"link": key,
-				"title": value["title"],
-				"desc": value["desc"],
-			});
-		}
-		// search description
-		else if (value["desc"].toLowerCase().includes(search)) {
-			resultList.push({
-				"link": key,
-				"title": value["title"],
-				"desc": value["desc"],
-			});
-		}
-		// search tags
-		else {
-			for (var i = 0; i < value["tags"].length; i++) {
-				if (value["tags"][i].toLowerCase().includes(search)) {
-					resultList.push({
-						"link": key,
-						"title": value["title"],
-						"desc": value["desc"],
-					});
-					break;
-				}
-			}
-		}
+	function SearchResult(index, link, title, tags, desc) {
+		this.index = index;
+		this.link = link;
+		this.title = title;
+		this.tags = tags;
+		this.desc = desc;
 	}
 
-	console.log(resultList);
+	function count(a, b) {
+		return a.toLowerCase().split(b.toLowerCase()).length - 1;
+	}
+
+	var resultList = [];
+
+	for (let [key, value] of index) {
+		var sr = new SearchResult(0, key, value["title"], value["tags"], value["desc"]);
+		var words = search.split(" ");
+
+		for (var i = 0; i < words.length; i++) {
+			var w = words[i];
+			// title search
+			var c = count(sr.title, w)
+			sr.index += 3 * c;
+
+			// search tags
+			for (var j = 0; j < sr.tags.length; j++) {
+				c = count(sr.tags[j], w)
+				sr.index += 2 * c
+			}
+
+			// search description
+			var c = count(sr.desc, w)
+			sr.index += 1 * c;
+		}
+
+		if (sr.index > 0) {
+			resultList.push(sr);
+		}
+	}
 
 	if (resultList.length == 0) {
 		displayResult(null);
 		return null;
 	}
 
+	resultList.sort((a, b) => b.index - a.index);
+
 	for (var i = 0; i < resultList.length; i++) {
-		console.log("item");
-		console.log(resultList[i]);
 		displayResult(resultList[i]);
 	}
 }
 
+// Get the search term from the URL
 function getSearchTerm() {
 	const params = new URLSearchParams(window.location.search);
 	const q = params.get("q");
 	return q
 }
 
+// Executes on a search, and directs the user the the search page with the search term.
 function loadSearch() {
 
 	const searchInput = document.getElementById("search-input");
