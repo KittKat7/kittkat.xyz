7387a801e9caed9576ac99b9f9350501db0d42a9
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-10-21 15:43:06 -0400

    Added spooky coloring

---
+++ src/z.lib/eventtheme.js
@@ -0,0 +1,14 @@
+
+(function main() {
+	const dt = new Date();
+	if (dt.getMonth() == 9 && dt.getDate() == 21) {
+		document.body.setAttribute("mode", "dark");
+		document.body.setAttribute("event", "halloween");
+		const style = document.createElement("link");
+		style.rel = "stylesheet";
+		style.href = "/z.lib/events.css";
+
+		document.head.appendChild(style);
+		// <link rel="stylesheet" href="/style.css?v=202508250154">
+	}
+})()
